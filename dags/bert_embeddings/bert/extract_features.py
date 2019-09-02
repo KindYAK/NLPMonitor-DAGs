c258b39b12fa530cf2e5c4574329637d962f1fd8
Yakunin Kirill <yakunin.k@mail.ru>
2019-09-02 10:07:46 +0600

    TF verbosity to ERROR

--- dags/bert_embeddings/bert/extract_features.py
+++ dags/bert_embeddings/bert/extract_features.py
@@ -341,7 +341,7 @@ def read_examples(input_file):
 
 
 def main(_):
-  tf.logging.set_verbosity(tf.logging.INFO)
+  tf.logging.set_verbosity(tf.logging.ERROR)
 
   layer_indexes = [int(x) for x in FLAGS.layers.split(",")]
 
