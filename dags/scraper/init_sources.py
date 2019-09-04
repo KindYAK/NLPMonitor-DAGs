b0ee63dc471391c8098e2133d695c58be02d4644
Yakunin Kirill <yakunin.k@mail.ru>
2019-09-04 11:03:40 +0600

    Scrapers - init_sources DAG

---
+++ dags/scraper/init_sources.py
@@ -0,0 +1,37 @@
+"""
+Code that goes along with the Airflow tutorial located at:
+https://github.com/apache/airflow/blob/master/airflow/example_dags/tutorial.py
+"""
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonVirtualenvOperator, PythonOperator
+from DjangoOperator import DjangoOperator
+from datetime import datetime, timedelta
+
+from dags.scraper.init_sources.service import init_sources, test
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 9, 4),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 5,
+    'retry_delay': timedelta(minutes=15),
+    'priority_weight': 75
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG('Scrapers_init_sources', default_args=default_args, schedule_interval='0 20 * * *')
+
+
+with dag:
+    init_sources = DjangoOperator(
+        task_id="init_sources",
+        python_callable=init_sources,
+    )
---
+++ dags/scraper/init_sources/service.py
@@ -0,0 +1,25 @@
+def init_sources(**kwargs):
+    from airflow.models import Variable
+    from mainapp.models import Source
+    import json
+
+    ss = Source.objects.all()
+    Variable.set("sources",
+                     json.dumps(
+                         [{
+                             "id": s.id,
+                             "name": s.name,
+                             "url": s.url
+                         } for s in ss]
+                     )
+                 )
+
+
+def test(**kwargs):
+    import json
+    from airflow.models import Variable
+
+    ss = json.loads(Variable.get('sources'))
+    print(ss)
+
+    return len(ss)
