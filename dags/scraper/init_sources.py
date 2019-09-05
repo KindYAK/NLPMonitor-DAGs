2ba34fa3291f7e828d13b881b25b8ddaa245e6e1
Yakunin Kirill <yakunin.k@mail.ru>
2019-09-05 17:27:19 +0600

    Long/short tasks pool division

--- dags/scraper/init_sources.py
+++ dags/scraper/init_sources.py
@@ -21,7 +21,7 @@ default_args = {
     'retries': 5,
     'retry_delay': timedelta(minutes=15),
     'priority_weight': 75,
-
+    'pool': 'short_tasks'
     # 'queue': 'bash_queue',
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
