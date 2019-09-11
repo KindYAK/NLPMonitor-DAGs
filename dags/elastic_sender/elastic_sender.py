8dd1c9723fdfbccf595d8d487c3af7ba4805e195
Yakunin Kirill <yakunin.k@mail.ru>
2019-09-11 14:38:08 +0600

    Send Elastic DAG

---
+++ dags/elastic_sender/elastic_sender.py
@@ -0,0 +1,37 @@
+"""
+Code that goes along with the Airflow tutorial located at:
+https://github.com/apache/airflow/blob/master/airflow/example_dags/tutorial.py
+"""
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonVirtualenvOperator, PythonOperator
+from DjangoOperator import DjangoOperator
+from datetime import datetime, timedelta
+
+from dags.elastic_sender.sender.service import send_elastic
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 9, 12),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 5,
+    'retry_delay': timedelta(minutes=15),
+    'priority_weight': 60,
+    'pool': 'long_tasks'
+    # 'queue': 'bash_queue',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG('Nlpmonitor_send_elastic', default_args=default_args, schedule_interval='59 23 * * *')
+
+
+with dag:
+    send_elastic = DjangoOperator(
+        task_id="send_elastic",
+        python_callable=send_elastic,
+    )
--- dags/elastic_sender/sender/service.py
+++ dags/elastic_sender/sender/service.py
@@ -1,2 +1,12 @@
 def send_elastic(**kwargs):
-    pass
+    from airflow.models import Variable
+    from mainapp.management.commands.build_search import Command
+    from mainapp.models import Document
+
+    from_id = Variable.get("send_elatic_from_id", default_var=0)
+    to_id = Document.objects.latest('id').id
+
+    # Send_elastic call
+    Command().handle(batch_size=1000, from_id=from_id, to_id=to_id)
+
+    Variable.set("send_elastic_from_id", to_id)
