3123e215caa30ee1a2e78d556f5bc0f25eca7a5b
Yakunin Kirill <yakunin.k@mail.ru>
2019-09-23 10:03:54 +0600

    Spider - time limit

--- dags/scraper/scrapy_project/scrapy_project/spiders/spider.py
+++ dags/scraper/scrapy_project/scrapy_project/spiders/spider.py
@@ -71,7 +71,7 @@ class TheSpider(scrapy.spiders.CrawlSpider):
                 self.last_depth = response.meta['depth'] + 1
             self.depth_history_depth = response.meta['depth']
             self.depth_history = []
-        if self.last_depth and response.meta['depth'] > self.last_depth or (datetime.datetime.now() - self.start_time).seconds > 48*60*60:
+        if self.last_depth and response.meta['depth'] > self.last_depth or (datetime.datetime.now() - self.start_time).seconds > 24*60*60:
             raise CloseSpider('No more new stuff')
 
         simple_fields = ("text", "title", "author", "datetime", "num_views", "num_likes", "num_comments", "num_shares", )
