b8cb24cd1d0b7814a086bbb2995bb985d1b8df41
Yakunin Kirill <yakunin.k@mail.ru>
2019-10-08 12:43:34 +0600

    Dictionary generator (interrim, need to also squash dicts from different threads)

---
+++ dags/generate_dictionary/generate_dictionary.py
@@ -0,0 +1,50 @@
+"""
+Code that goes along with the Airflow tutorial located at:
+https://github.com/apache/airflow/blob/master/airflow/example_dags/tutorial.py
+"""
+from airflow import DAG
+from DjangoOperator import DjangoOperator
+from datetime import datetime, timedelta
+
+from dags.generate_dictionary.services.service import generate_dictionary_batch, init_dictionary_index
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 10, 3),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=15),
+    'priority_weight': 50,
+    'pool': 'long_tasks'
+}
+
+dag = DAG('Nlpmonitor_Dictionary_Generation', default_args=default_args, schedule_interval=None)
+
+with dag:
+    init_dictionary_index = DjangoOperator(
+        task_id="init_dictionary_index",
+        python_callable=init_dictionary_index,
+        op_kwargs={
+            "corpus": "main",
+            "name": "default_dict_pymorphy_2_4_393442_3710985",
+            "datetime": datetime.now(),
+        }
+    )
+
+    concurrency = 4
+    dictionary_operators = []
+    for i in range(concurrency):
+        dictionary_operators.append(DjangoOperator(
+            task_id=f"dictionary_{i}",
+            python_callable=generate_dictionary_batch,
+            op_kwargs={
+                "name": "default_dict_pymorphy_2_4_393442_3710985",
+                "i": i,
+                "start": (100 / concurrency) * i,
+                "end": (100 / concurrency) * (i + 1)
+            }
+        ))
+    init_dictionary_index >> dictionary_operators
---
+++ dags/generate_dictionary/services/service.py
@@ -0,0 +1,85 @@
+def init_dictionary_index(**kwargs):
+    from elasticsearch_dsl import Search
+
+    from nlpmonitor.settings import ES_CLIENT, ES_INDEX_DOCUMENT, ES_INDEX_DICTIONARY
+    from mainapp.documents import Dictionary
+
+    from util.service_es import search
+
+    s = Search(using=ES_CLIENT, index=ES_INDEX_DOCUMENT)
+    number_of_documents = s.count()
+
+    # Check if already exists
+    if ES_CLIENT.indices.exists(ES_INDEX_DICTIONARY):
+        query = {
+            "corpus": kwargs['corpus'],
+            "name": kwargs['name'],
+            "number_of_documents": number_of_documents,
+        }
+        if search(ES_CLIENT, ES_INDEX_DICTIONARY, query):
+            return ("!!!", "Already exists")
+
+    kwargs["number_of_documents"] = number_of_documents
+    dictionary = Dictionary(**kwargs)
+    dictionary.save()
+
+
+def generate_dictionary_batch(**kwargs):
+    import re
+    from util.service_es import search, update_generator
+    from elasticsearch_dsl import Search
+    from nlpmonitor.settings import ES_INDEX_DOCUMENT, ES_INDEX_DICTIONARY, ES_CLIENT
+    from elasticsearch.helpers import streaming_bulk
+    from stop_words import get_stop_words
+    from pymorphy2 import MorphAnalyzer
+
+    name = kwargs['name']
+    i = kwargs['i']
+    start = kwargs['start']
+    end = kwargs['end']
+
+    query = {
+        "name": name,
+    }
+    dictionary = search(ES_CLIENT, ES_INDEX_DICTIONARY, query)[-1]
+    number_of_documents = dictionary['number_of_documents']
+    if not number_of_documents:
+        raise Exception("No variable!")
+
+    s = search(ES_CLIENT, ES_INDEX_DOCUMENT, query={}, source=['text'], sort=['id'], get_search_obj=True,
+                       start=int(start/100*number_of_documents), end=int(end/100*number_of_documents)+1)
+    documents = s.execute()
+
+    stopwords = get_stop_words('ru')
+    morph = MorphAnalyzer()
+    dictionary_words = {}
+    for doc in documents:
+        word_in_doc = set()
+        cleaned_words = (x.lower() for x in ' '.join(re.sub('([^А-Яа-яa-zA-Z0-9 ]|[^ ]*[*][^ ]*)', ' ', doc.text).split()).split())
+        for word in cleaned_words:
+            if word not in dictionary_words:
+                parse = morph.parse(word)
+                dictionary_words[word] = {
+                    "word": word,
+                    "word_normal": parse[0].normal_form,
+                    "is_in_pymorphy2_dict": parse[0].is_known,
+                    "is_multiple_normals_in_pymorphy2": len(parse) > 1,
+                    "is_stop_word": word in stopwords or parse[0].normal_form in stopwords,
+                    "is_latin": any([c in "qwertyuiopasdfghjklzxcvbnm" for c in word]),
+                    "is_kazakh": any([c in "ӘәҒғҚқҢңӨөҰұҮүІі" for c in word]),
+                    "pos_tag": parse[0].tag.POS,
+                    "word_len": len(word),
+                    "word_frequency": 1,
+                    "document_frequency": 1,
+                }
+            else:
+                dictionary_words[word]['word_frequency'] += 1
+                if word not in word_in_doc:
+                    dictionary_words[word]['document_frequency'] += 1
+            word_in_doc.add(word)
+
+    for ok, result in streaming_bulk(ES_CLIENT, update_generator(ES_INDEX_DICTIONARY, [dictionary], {f"words_{i}": list(dictionary_words.values())}),
+                                     index=ES_INDEX_DOCUMENT,
+                                     chunk_size=1000, raise_on_error=True, max_retries=10):
+        pass
+    return len(documents)
