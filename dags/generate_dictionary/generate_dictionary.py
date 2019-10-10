5457aa17728387d9e5e4c3f685b3306bac8349ef
Yakunin Kirill <yakunin.k@mail.ru>
2019-10-10 11:16:25 +0600

    Dict - concurrency increase

--- dags/generate_dictionary/generate_dictionary.py
+++ dags/generate_dictionary/generate_dictionary.py
@@ -34,7 +34,7 @@ with dag:
         }
     )
 
-    concurrency = 4
+    concurrency = 24
     dictionary_operators = []
     for i in range(concurrency):
         dictionary_operators.append(DjangoOperator(
