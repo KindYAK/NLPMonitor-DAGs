94b8a325a14ecf7340991a5cbbeb05b9802acd88
Yakunin Kirill <yakunin.k@mail.ru>
2019-10-14 15:03:54 +0600

    Dictionary generation - First_capital ratio

--- dags/generate_dictionary/services/service.py
+++ dags/generate_dictionary/services/service.py
@@ -54,8 +54,10 @@ def generate_dictionary_batch(**kwargs):
     dictionary_words = {}
     for doc in s.execute():
         word_in_doc = set()
-        cleaned_words = (x.lower() for x in ' '.join(re.sub('([^А-Яа-яa-zA-ZӘәҒғҚқҢңӨөҰұҮүІі]|[^ ]*[*][^ ]*)', ' ', doc.text).split()).split())
+        cleaned_words = (x for x in ' '.join(re.sub('([^А-Яа-яa-zA-ZӘәҒғҚқҢңӨөҰұҮүІі]|[^ ]*[*][^ ]*)', ' ', doc.text).split()).split())
         for word in cleaned_words:
+            is_first_upper = word[0].isupper()
+            word = word.lower()
             if word not in dictionary_words:
                 parse = morph.parse(word)
                 dictionary_words[word] = {
@@ -71,9 +73,11 @@ def generate_dictionary_batch(**kwargs):
                     "word_len": len(word),
                     "word_frequency": 1,
                     "document_frequency": 1,
+                    "word_first_capital_ratio": 1 if is_first_upper else 0,
                 }
             else:
                 dictionary_words[word]['word_frequency'] += 1
+                dictionary_words[word]['word_first_capital_ratio'] += 1 if word[0].isupper() else 0
                 if word not in word_in_doc:
                     dictionary_words[word]['document_frequency'] += 1
             word_in_doc.add(word)
@@ -91,11 +95,11 @@ def aggregate_dicts(**kwargs):
     from nlpmonitor.settings import ES_INDEX_DICTIONARY_INDEX, ES_INDEX_DICTIONARY_WORD, ES_CLIENT
 
     name = kwargs['name']
-
     query = {
         "dictionary": name,
     }
     dictionary = search(ES_CLIENT, ES_INDEX_DICTIONARY_WORD + "_temp", query, get_search_obj=True)
+    dictionary_index = search(ES_CLIENT, ES_INDEX_DICTIONARY_INDEX, {"name": name})[-1]
     dictionary_words_final = {}
     dictionary_normal_words = {}
     for word in dictionary.scan():
@@ -105,24 +109,36 @@ def aggregate_dicts(**kwargs):
             dictionary_words_final[key] = word.to_dict()
         else:
             dictionary_words_final[key]['word_frequency'] += word['word_frequency']
+            dictionary_words_final[key]['word_first_capital_ratio'] += word['word_first_capital_ratio']
             dictionary_words_final[key]['document_frequency'] += word['document_frequency']
+
         if not key_normal in dictionary_normal_words:
             dictionary_normal_words[key_normal] = {
                 "word_normal_frequency": word['word_frequency'],
+                "word_normal_first_capital_ratio": word['word_first_capital_ratio'],
                 "document_normal_frequency": word['document_frequency']
             }
         else:
             dictionary_normal_words[key_normal]['word_normal_frequency'] += word['word_frequency']
+            dictionary_normal_words[key_normal]['word_normal_first_capital_ratio'] += word['word_first_capital_ratio']
             dictionary_normal_words[key_normal]['document_normal_frequency'] += word['document_frequency']
     for key in dictionary_words_final.keys():
         dictionary_words_final[key]['word_normal_frequency'] = dictionary_normal_words[dictionary_words_final[key]['word_normal']]['word_normal_frequency']
+        dictionary_words_final[key]['word_normal_first_capital_ratio'] = dictionary_normal_words[dictionary_words_final[key]['word_normal']]['word_normal_first_capital_ratio']
         dictionary_words_final[key]['document_normal_frequency'] = dictionary_normal_words[dictionary_words_final[key]['word_normal']]['document_normal_frequency']
+
+        dictionary_words_final[key]['word_first_capital_ratio'] /= dictionary_words_final[key]['word_frequency']
+        dictionary_words_final[key]['word_normal_first_capital_ratio'] /= dictionary_words_final[key]['word_normal_frequency']
+
+        dictionary_words_final[key]['word_frequency_relative'] = dictionary_words_final[key]['word_frequency'] / dictionary_index.number_of_documents
+        dictionary_words_final[key]['word_normal_frequency_relative'] = dictionary_words_final[key]['word_normal_frequency'] / dictionary_index.number_of_documents
+        dictionary_words_final[key]['document_frequency_relative'] = dictionary_words_final[key]['document_frequency'] / dictionary_index.number_of_documents
+        dictionary_words_final[key]['document_normal_frequency_relative'] = dictionary_words_final[key]['document_normal_frequency'] / dictionary_index.number_of_documents
 
     for ok, result in parallel_bulk(ES_CLIENT, dictionary_words_final.values(),
                                      index=ES_INDEX_DICTIONARY_WORD,
                                      chunk_size=10000, raise_on_error=True, thread_count=4):
         pass
 
-    dictionary_index = search(ES_CLIENT, ES_INDEX_DICTIONARY_INDEX, {"name": name})[-1]
     ES_CLIENT.update(index=ES_INDEX_DICTIONARY_INDEX, id=dictionary_index.meta.id, body={"doc": {"is_ready": True}})
     return 0
