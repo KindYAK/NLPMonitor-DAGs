5a4563b8e78d2ccb44e347c2a23ce5376bd8bd97
Yakunin Kirill <yakunin.k@mail.ru>
2019-12-25 15:57:10 +0600

    Fix

--- dags/bigartm/services/calc_topics_info.py
+++ dags/bigartm/services/calc_topics_info.py
@@ -64,7 +64,7 @@ def calc_topics_info(corpus, topic_modelling_name, topic_weight_threshold):
                 is_up = True
                 period_start = i
             elif weight < topic.weight_geom_mean and is_up:
-                if max_up - topic.weight_mean > 0.1 * topic.weight_std:
+                if max_up - topic.weight_mean > 0.5 * topic.weight_std:
                     periods.append(i - period_start)
                     periods_maxes.append(max_up)
                 is_up = False
