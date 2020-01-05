ed5470ea7f31465d0bc5e3a6e6e0c24c08062e4a
Yakunin Kirill <yakunin.k@mail.ru>
2020-01-05 17:43:06 +0600

    Calc virt negative field to init_criterions task

--- dags/criterion_eval/init_criterions/service.py
+++ dags/criterion_eval/init_criterions/service.py
@@ -16,6 +16,7 @@ def init_criterions(**kwargs):
                              "value_range_to": criterion.value_range_to,
                              "is_integer": criterion.is_integer,
                              "is_categorical": criterion.is_categorical,
+                             "calc_virt_negative": criterion.calc_virt_negative,
                              "topic_modellings": list(set([topic_id.topic_modelling_name for topic_id in TopicID.objects.filter(topicseval__criterion=criterion).order_by("id")])),
                              "topic_modellings_translit": list(set([translit(topic_id.topic_modelling_name, 'ru', reversed=True)
                                                                     for topic_id in TopicID.objects.filter(topicseval__criterion=criterion).order_by("id")])),
