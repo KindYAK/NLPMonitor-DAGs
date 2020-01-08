7ed71dc0bfaa3c2a5d618ac0f9ac89d910faf4d5
Yakunin Kirill <yakunin.k@mail.ru>
2020-01-08 13:56:02 +0600

    Calc eval - aaaaand back to generators :D with all previous changes and optimisations

--- dags/elastic_sender/elastic_sender.py
+++ dags/elastic_sender/elastic_sender.py
@@ -21,7 +21,7 @@ default_args = {
     'retries': 3,
     'retry_delay': timedelta(minutes=15),
     'priority_weight': 80,
-    'pool': 'long_tasks'
+    'pool': 'short_tasks'
     # 'queue': 'bash_queue',
     # 'priority_weight': 10,
     # 'end_date': datetime(2016, 1, 1),
