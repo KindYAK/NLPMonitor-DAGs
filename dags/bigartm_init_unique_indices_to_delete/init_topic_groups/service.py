3c05901854c9658bdf6c0e887810e4ab54562c18
Yakunin Kirill <yakunin.k@mail.ru>
2020-01-13 17:24:01 +0600

    Temporary DAG - initializatio of unique ID indices for TM

---
+++ dags/bigartm_init_unique_indices_to_delete/init_topic_groups/service.py
@@ -0,0 +1,30 @@
+def transliterate_for_dag_id(name):
+    from transliterate import translit
+    name_translit = translit(name, 'ru', reversed=True)
+    return clear_symbols(name_translit)
+
+
+def clear_symbols(name):
+    return "".join([c for c in name.replace(" ", "_") if (c.isalnum() or c in ["_", ".", "-"]) and c not in "әғқңөұүі"]).strip().lower()
+
+
+def init_topic_groups(**kwargs):
+    from airflow.models import Variable
+    from mainapp.models_user import TopicGroup
+    import json
+
+    groups = TopicGroup.objects.all()
+    Variable.set("topic_groups",
+                 json.dumps(
+                     [{
+                         "id": group.id,
+                         "name": clear_symbols(group.name),
+                         "name_translit": transliterate_for_dag_id(group.name),
+                         "topic_modelling_name": group.topic_modelling_name,
+                         "topic_modelling_name_translit": transliterate_for_dag_id(group.topic_modelling_name),
+                         "topics": [topic.topic_id for topic in group.topics.all()],
+                         "owner": group.owner.username,
+                         "is_public": group.is_public,
+                     } for group in groups]
+                 )
+                 )
