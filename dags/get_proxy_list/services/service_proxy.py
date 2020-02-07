221ae13fcc58deb0373a01acb767d676dce83470
Yakunin Kirill <yakunin.k@mail.ru>
2020-02-07 15:28:18 +0600

    TEMP - scraper - more proxies and retreis for Forbes

--- dags/get_proxy_list/services/service_proxy.py
+++ dags/get_proxy_list/services/service_proxy.py
@@ -6,8 +6,8 @@ def get_proxy_list():
 
     from util.constants import BASE_DAG_DIR
 
-    async def write_to_file(proxies):
-        with open(os.path.join(BASE_DAG_DIR, "proxy_list.txt"), "w") as f:
+    async def write_to_file(proxies, mode="w"):
+        with open(os.path.join(BASE_DAG_DIR, "proxy_list.txt"), mode) as f:
             while True:
                 proxy = await proxies.get()
                 if proxy is None:
@@ -22,10 +22,28 @@ def get_proxy_list():
                 ('HTTP', ('Anonymous', 'High')),
                 ('HTTPS', ('Anonymous', 'High'))
             ],
-            limit=1000
+            limit=100
         ),
         write_to_file(proxies)
     )
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(tasks)
+
+    for i in range(24):
+        print("Writing", i)
+        proxies = asyncio.Queue()
+        broker = Broker(proxies)
+        tasks = asyncio.gather(
+            broker.find(
+                types=[
+                    ('HTTP', ('Anonymous', 'High')),
+                    ('HTTPS', ('Anonymous', 'High'))
+                ],
+                limit=100
+            ),
+            write_to_file(proxies)
+        )
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(tasks)
