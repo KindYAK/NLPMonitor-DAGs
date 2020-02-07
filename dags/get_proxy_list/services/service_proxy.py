e899bdfbb259de574cc0939e36d0a3eeea44402d
Yakunin Kirill <yakunin.k@mail.ru>
2020-02-07 16:16:37 +0600

    Get proxy list - Get all proxies

--- dags/get_proxy_list/services/service_proxy.py
+++ dags/get_proxy_list/services/service_proxy.py
@@ -6,8 +6,8 @@ def get_proxy_list():
 
     from util.constants import BASE_DAG_DIR
 
-    async def write_to_file(proxies):
-        with open(os.path.join(BASE_DAG_DIR, "proxy_list.txt"), "w") as f:
+    async def write_to_file(proxies, mode="w"):
+        with open(os.path.join(BASE_DAG_DIR, "proxy_list.txt"), mode) as f:
             while True:
                 proxy = await proxies.get()
                 if proxy is None:
@@ -22,7 +22,7 @@ def get_proxy_list():
                 ('HTTP', ('Anonymous', 'High')),
                 ('HTTPS', ('Anonymous', 'High'))
             ],
-            limit=600
+            limit=0
         ),
         write_to_file(proxies)
     )
