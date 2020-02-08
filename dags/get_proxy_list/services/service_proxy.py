8db64f6774c44a2aff822b863b6edd142eb5942b
Yakunin Kirill <yakunin.k@mail.ru>
2020-02-08 20:08:25 +0600

    Proxy get list - attempt to solve de-sync, when proxy list turns out to be empty for scrapers

--- dags/get_proxy_list/services/service_proxy.py
+++ dags/get_proxy_list/services/service_proxy.py
@@ -6,16 +6,16 @@ def get_proxy_list():
 
     from util.constants import BASE_DAG_DIR
 
-    async def write_to_file(proxies, mode="w"):
-        with open(os.path.join(BASE_DAG_DIR, "proxy_list.txt"), mode) as f:
-            while True:
-                proxy = await proxies.get()
-                if proxy is None:
-                    break
-                f.write(f"http://{proxy.host}:{proxy.port}\n")
+    async def get_list(proxies, proxy_list):
+        while True:
+            proxy = await proxies.get()
+            if proxy is None:
+                break
+            proxy_list.append(f"http://{proxy.host}:{proxy.port}\n")
 
     proxies = asyncio.Queue()
     broker = Broker(proxies)
+    proxy_list = []
     tasks = asyncio.gather(
         broker.find(
             types=[
@@ -24,8 +24,12 @@ def get_proxy_list():
             ],
             limit=250
         ),
-        write_to_file(proxies)
+        get_list(proxies, proxy_list)
     )
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(tasks)
+
+    with open(os.path.join(BASE_DAG_DIR, "proxy_list.txt"), "w") as f:
+        for proxy in proxy_list:
+            f.write(f"{proxy}\n")
