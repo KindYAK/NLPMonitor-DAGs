942449b11681f9a5533f97f7035b6e521d96a8d7
Sanzhar <sanzharmrz@gmail.com>
2020-02-19 11:38:29 +0600

    feat dynamic_tm: add callable for meta dtm creation

--- dags/dynamic_tm/services/meta_dtm_creator.py
+++ dags/dynamic_tm/services/meta_dtm_creator.py
@@ -4,15 +4,15 @@ def generate_meta_dtm(**kwargs):
     from util.service_es import search
     from elasticsearch_dsl import Index
 
-    meta_name = kwargs['meta_name']
-    volume_days = kwargs['volume_days']
+    meta_name = kwargs['meta_dtm_name']
+    volume_days = kwargs['tm_volume_days']
     delta_days = kwargs['delta_days']
     reset_index = kwargs['reset_index']
+    from_date = kwargs['from_date']
+    to_date = kwargs['to_date']
 
     if reset_index:
-        index = Index(ES_INDEX_META_DTM)
-        index.delete(using=ES_CLIENT, ignore=404)
-
+        Index(ES_INDEX_META_DTM).delete(using=ES_CLIENT, ignore=404)
 
     if not ES_CLIENT.indices.exists(ES_INDEX_META_DTM):
         ES_CLIENT.indices.create(index=ES_INDEX_META_DTM, body={
@@ -21,7 +21,8 @@ def generate_meta_dtm(**kwargs):
         })
 
     s = search(client=ES_CLIENT, index=ES_INDEX_META_DTM,
-               query={'meta_name': meta_name, 'volume_days': volume_days, 'delta_days': delta_days})
+               query={'meta_name': meta_name, 'volume_days': volume_days, 'delta_days': delta_days,
+                      'from_date': from_date, 'to_date': to_date})
 
     if s:
         ES_CLIENT.update(index=ES_INDEX_META_DTM, id=s[-1].meta.id,
@@ -31,6 +32,9 @@ def generate_meta_dtm(**kwargs):
                                  "meta_name": meta_name,
                                  "volume_days": volume_days,
                                  "delta_days": delta_days,
+                                 'from_date': from_date,
+                                 'to_date': to_date,
+                                 'reset_index': reset_index
                              }
                          }
                          )
