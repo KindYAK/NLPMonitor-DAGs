7247ebc57a965f5e49a0b1a2de0cc511fa6e2db3
Yakunin Kirill <yakunin.k@mail.ru>
2020-02-24 10:24:10 +0600

    Astana cluster test - refactoring, checking ES connection with basic auth

--- dags/astana_test/external_file_example/my_package.py
+++ dags/astana_test/external_file_example/my_package.py
@@ -1,6 +1,11 @@
 def test():
+    from nlpmonitor.settings import ES_CLIENT
     from mainapp.models import Corpus
-    print("!!!", Corpus.objects.count())
+
+    from elasticsearch_dsl import Search, ES_INDEX_DOCUMENT
+
+    print("!!! Checking DB access", Corpus.objects.count())
+    print("!!! Checking ElasticSearch access", Search(using=ES_CLIENT, index=ES_INDEX_DOCUMENT).count())
     import xlrd
-    print(xlrd.__version__)
-    return xlrd.__version__
+    print("!!! Checking venv access", xlrd.__version__)
+    return "OK"
