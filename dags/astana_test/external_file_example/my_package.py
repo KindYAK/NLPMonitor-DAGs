c7f1781fbb24ba34b000b45339515cc3e1424185
Yakunin Kirill <yakunin.k@mail.ru>
2020-02-24 12:29:25 +0600

    Fix

--- dags/astana_test/external_file_example/my_package.py
+++ dags/astana_test/external_file_example/my_package.py
@@ -1,8 +1,8 @@
 def test():
-    from nlpmonitor.settings import ES_CLIENT
+    from nlpmonitor.settings import ES_CLIENT, ES_INDEX_DOCUMENT
     from mainapp.models import Corpus
 
-    from elasticsearch_dsl import Search, ES_INDEX_DOCUMENT
+    from elasticsearch_dsl import Search
 
     print("!!! Checking DB access", Corpus.objects.count())
     print("!!! Checking ElasticSearch access", Search(using=ES_CLIENT, index=ES_INDEX_DOCUMENT).count())
