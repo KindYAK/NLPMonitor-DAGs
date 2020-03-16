e42c4d83c73fbbcf150beef223579a9c4b9b29c6
Sanzhar <sanzharmrz@gmail.com>
2020-03-16 16:49:29 +0600

    feat preprocessor: add lemmatizer for latin texts

--- dags/tm_preprocessing/services/tm_preproc_services.py
+++ dags/tm_preprocessing/services/tm_preproc_services.py
@@ -34,6 +34,8 @@ def preprocessing_raw_data(**kwargs):
     from stop_words import get_stop_words
     from pymorphy2 import MorphAnalyzer
     from airflow.models import Variable
+    from nltk.stem import WordNetLemmatizer
+    from util.util import is_latin
 
     start = kwargs['start']
     end = kwargs['end']
@@ -45,7 +47,10 @@ def preprocessing_raw_data(**kwargs):
     documents = search(ES_CLIENT, ES_INDEX_DOCUMENT, query={}, source=['text'], sort=['id'], get_scan_obj=True,
                        start=int(start/100*number_of_documents), end=int(end/100*number_of_documents)+1).exclude('exists', field="text_lemmatized")
 
-    stopwords = get_stop_words('ru')
+    stopwords_ru = get_stop_words('ru')
+    stopwords_eng = get_stop_words('en')
+
+    lemmatizer = WordNetLemmatizer()
     morph = MorphAnalyzer()
 
     s = Search(using=ES_CLIENT, index=ES_INDEX_CUSTOM_DICTIONARY_WORD)
@@ -54,7 +59,14 @@ def preprocessing_raw_data(**kwargs):
 
     for doc in documents:
         cleaned_doc = " ".join(x.lower() for x in ' '.join(re.sub('([^А-Яа-яa-zA-ZӘәҒғҚқҢңӨөҰұҮүІі-]|[^ ]*[*][^ ]*)', ' ', doc.text).split()).split())
-        cleaned_words_list = [morph_with_dictionary(morph, word, custom_dict) for word in cleaned_doc.split() if len(word) > 2 and word not in stopwords]
+        if is_latin(cleaned_doc):
+            cleaned_words_list = [lemmatizer.lemmatize(word) for word in cleaned_doc.split() if
+                                  len(word) >= 2 and word not in stopwords_eng]
+
+        else:
+            cleaned_words_list = [morph_with_dictionary(morph, word, custom_dict) for word in cleaned_doc.split() if
+                                  len(word) > 2 and word not in stopwords_ru]
+
         cleaned_words_list = [word for word in cleaned_words_list if len(word) > 2]
         cleaned_doc = " ".join(cleaned_words_list)
         doc['text_lemmatized'] = cleaned_doc
