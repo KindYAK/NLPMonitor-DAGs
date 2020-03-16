0e49aaaaa2910649e48c199bc20dd77feedd59f5
Sanzhar <sanzharmrz@gmail.com>
2020-03-16 16:49:49 +0600

    feat bigartm: add dag6 with scientometrics

--- dags/bigartm/bigartm.py
+++ dags/bigartm/bigartm.py
@@ -381,3 +381,24 @@ with dag5:
                         "DecorrelatorPhiRegularizer": 0.15,
                         "ImproveCoherencePhiRegularizer": 0.15
                     }, is_actualizable=True)
+
+dag6 = DAG('NLPmonitor_BigARTMs_scientometrics', catchup=False, max_active_runs=1, default_args=default_args, schedule_interval=None)
+with dag6:
+    wait_for_basic_tms = PythonOperator(
+        task_id="wait_for_basic_tms",
+        python_callable=lambda: 0,
+    )
+
+    gen_bigartm_operator(name=f"bigartm_two_years_scientometrics", description="scientometrics 17k", number_of_topics=50,
+                         filters={
+                        "corpus": "scientometrics",
+                        "source": None,
+                        "datetime_from": date(2004, 1, 1),
+                        "datetime_to": date(2020, 4, 1),
+                    },
+                    regularization_params={
+                        "SmoothSparseThetaRegularizer": 0.15,
+                        "SmoothSparsePhiRegularizer": 0.15,
+                        "DecorrelatorPhiRegularizer": 0.15,
+                        "ImproveCoherencePhiRegularizer": 0.15
+                    }, is_actualizable=True)
