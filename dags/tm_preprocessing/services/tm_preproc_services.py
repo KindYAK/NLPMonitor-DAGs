88cffad0dd172209bc9c8f79ef3522806146777e
Sanzhar <sanzharmrz@gmail.com>
2020-03-17 17:49:11 +0600

    fix tm preproc service: add filter for scientometrics

--- dags/tm_preprocessing/services/tm_preproc_services.py
+++ dags/tm_preprocessing/services/tm_preproc_services.py
@@ -46,7 +46,7 @@ def preprocessing_raw_data(**kwargs):
         raise Exception("No variable!")
 
     documents = search(ES_CLIENT, ES_INDEX_DOCUMENT, query={}, source=['text'], sort=['id'], get_scan_obj=True,
-                       start=int(start/100*number_of_documents), end=int(end/100*number_of_documents)+1).exclude('exists', field="text_lemmatized")
+                       start=int(start/100*number_of_documents), end=int(end/100*number_of_documents)+1).filter('term', **{'corpus':'scientometrics'})
 
     stopwords_ru = get_stop_words('ru')
     stopwords_eng = get_stop_words('en') + stopwords.words('english')
