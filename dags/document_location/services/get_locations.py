0199cbfadd4e55ab41be54c84a77b57a43ec1d90
Sanzhar <sanzharmrz@gmail.com>
2020-04-10 18:54:54 +0600

    feat get_locations: add right criterion calculating

--- dags/document_location/services/get_locations.py
+++ dags/document_location/services/get_locations.py
@@ -1,59 +1,67 @@
-def get_locations(**kwargs):
-    from collections import defaultdict
+def locations_generator(**kwargs):
 
-    from geo.models import Area
-    from eval.models import EvalCriterion
+    from geo.models import Area, District, Locality
     from mainapp.documents import DocumentLocation
-    from mainapp.constants import SEARCH_CUTOFF_CONFIG
+    from nlpmonitor.settings import ES_CLIENT, ES_INDEX_DOCUMENT, ES_INDEX_DOCUMENT_LOCATION, ES_INDEX_DOCUMENT_EVAL
 
     from elasticsearch_dsl import Search, Q
 
-    from nlpmonitor.settings import ES_CLIENT, ES_INDEX_DOCUMENT, \
-        ES_INDEX_TOPIC_DOCUMENT
+    criterion_tm_duos = kwargs['criterion_tm_duos']  # ((tm_1, criterion_id_1)....()...())
 
-    topic_modelling = kwargs['topic_modelling']
-    perform_actualize = 'perform_actualize' in kwargs
-    criterion = EvalCriterion.objects.get(id=kwargs['criterion_id'])
-    # TODO как кошерно заюзать criterion
-    doc_locations = list()
-    for places in (Area):  # , District, Locality):
+    for places in (Area, District, Locality):
         location_level = places.objects.first()._meta.verbose_name
         for geo in places.objects.all():
-            s = Search(using=ES_CLIENT, index=ES_INDEX_DOCUMENT).source(['text', 'text_lemmatized', 'title'])
-            Q(
+            s = Search(using=ES_CLIENT, index=ES_INDEX_DOCUMENT).source(['_id', 'text', 'text_lemmatized', 'title'])
+            q = Q(
                 'bool',
                 should=[Q("match_phrase", text_lemmatized=geo.name)] +
                        [Q("match_phrase", text=geo.name)] +
                        [Q("match_phrase", title=geo.name)],
                 minimum_should_match=1,
             )
-            s = s.query(Q)
-            s = s[:SEARCH_CUTOFF_CONFIG["SEARCH_LVL_HARD"]['ABS_MAX_RESULTS_CUTOFF']]
-            documents_es_id = s.execute()
+            s = s.query(q)
+            scans = s.scan()
 
-            for document_es_id in documents_es_id:
-                topic_doc = Search(using=ES_CLIENT, index=f"{ES_INDEX_TOPIC_DOCUMENT}_{topic_modelling}") \
-                    .filter("range", topic_weight={"gte": 0.001}).filter("term", document_es_id=document_es_id) \
-                    .source(['topic_weight', "datetime", "document_source"])
-                data = defaultdict(int)
-                # TODO почему тут просто execute
-                for td in topic_doc.execute():
-                    data["datetime"] = td.datetime
-                    # TODO кошерно считать этот location weight
-                    data["eval_value"] += td.topic_weight
-                    data["source"] = td.document_source
-                    data["total_topic_weight"] += 1
+            for scan_obj in scans:
+                for tm, criterion_id in criterion_tm_duos:
+                    evaluated_docs = Search(using=ES_CLIENT, index=f"{ES_INDEX_DOCUMENT_EVAL}_{tm}_{criterion_id}") \
+                        .filter("term", document_es_id=scan_obj.meta.id) \
+                        .source(['value', 'document_datetime', 'document_source']) \
+                        .execute()
 
-                doc_locations.append(
-                    DocumentLocation(
-                        document_es_id=document_es_id,
-                        document_datetime=data["datetime"],
-                        document_source=data["source"],
-                        location_name=geo.name,
-                        location_level=location_level,
-                        criterion=criterion,
-                        location_weight=data["eval_value"] / data["total_topic_weight"]
-                    )
-                )
+                    if not evaluated_docs:
+                        continue
 
-    # TODO parallel bulk
+                    evaluated_docs = evaluated_docs[0]
+
+                    yield DocumentLocation(
+                            document_es_id=scan_obj.meta.id,
+                            document_datetime=evaluated_docs.document_datetime,
+                            document_source=evaluated_docs.document_source,
+                            location_name=geo.name,
+                            location_level=location_level,
+                            criterion_value=evaluated_docs.value,
+                            location_weight=1,  # TODO считать эти значения
+                            topic_modelling=tm
+                        )
+
+
+def get_locations(**kwargs):
+    from elasticsearch.helpers import parallel_bulk
+    from nlpmonitor.settings import ES_CLIENT, ES_INDEX_DOCUMENT_LOCATION
+
+    failed = 0
+    success = 0
+
+    for ok, result in parallel_bulk(ES_CLIENT, (doc.to_dict() for doc in locations_generator(**kwargs)),
+                                    index=ES_INDEX_DOCUMENT_LOCATION,
+                                    chunk_size=10000, raise_on_error=True, thread_count=4):
+
+        if failed > 5:
+            raise Exception("Too many failed ES!!!")
+        if not ok:
+            failed += 1
+        else:
+            success += 1
+
+    return 'Done'
