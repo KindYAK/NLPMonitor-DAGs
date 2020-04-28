212b4bc9ddb094fe8505a030a82485af15f7b157
Yakunin Kirill <yakunin.k@mail.ru>
2020-04-28 23:20:24 +0600

    change - bigartm - ngramized TMs

--- dags/bigartm/bigartm.py
+++ dags/bigartm/bigartm.py
@@ -547,3 +547,67 @@ with dag6:
                         },
                         wait_for_basic_tms=wait_for_basic_tms,
                         is_actualizable=True)
+
+
+dag7 = DAG('NLPmonitor_BigARTMs_ngramized', catchup=False, max_active_runs=1, concurrency=7, default_args=default_args, schedule_interval=None)
+with dag2:
+    wait_for_basic_tms = PythonOperator(
+        task_id="wait_for_basic_tms",
+        python_callable=lambda: 0,
+    )
+
+    gen_bigartm_operator(name="bigartm_test_ngram", description="Two last years", number_of_topics=200,
+                         filters={
+                             "corpus": "main",
+                             "source": None,
+                             "datetime_from": None,
+                             "datetime_to": None,
+                         },
+                         regularization_params={
+                             "SmoothSparseThetaRegularizer": 0.15,
+                             "SmoothSparsePhiRegularizer": 0.15,
+                             "DecorrelatorPhiRegularizer": 0.15,
+                             "ImproveCoherencePhiRegularizer": 0.15
+                         },
+                         wait_for_basic_tms=wait_for_basic_tms,
+                         is_actualizable=True,
+                         text_field="kz_rus_ngrams_dict_pymorphy_2_4_393442_3710985",
+                         )
+
+    gen_bigartm_operator(name="bigartm_two_years_ngram", description="Two last years", number_of_topics=200,
+                    filters={
+                        "corpus": "main",
+                        "source": None,
+                        "datetime_from": date(2017, 11, 1),
+                        "datetime_to": date(2019, 12, 1),
+                    },
+                    regularization_params={
+                        "SmoothSparseThetaRegularizer": 0.15,
+                        "SmoothSparsePhiRegularizer": 0.15,
+                        "DecorrelatorPhiRegularizer": 0.15,
+                        "ImproveCoherencePhiRegularizer": 0.15
+                    },
+                    wait_for_basic_tms=wait_for_basic_tms,
+                    is_actualizable=True,
+                    text_field="kz_rus_ngrams_dict_pymorphy_2_4_393442_3710985",
+                    )
+
+    gen_bigartm_operator(name="bigartm_education_two_years_ngram", description="Two last years education", number_of_topics=150,
+                         filters={
+                        "corpus": "main",
+                        "source": None,
+                        "datetime_from": date(2017, 11, 1),
+                        "datetime_to": date(2019, 12, 1),
+                        "group_id": 7,
+                        "topic_weight_threshold": 0.05,
+                    },
+                    regularization_params={
+                        "SmoothSparseThetaRegularizer": 0.15,
+                        "SmoothSparsePhiRegularizer": 0.15,
+                        "DecorrelatorPhiRegularizer": 0.15,
+                        "ImproveCoherencePhiRegularizer": 0.15
+                    },
+                    wait_for_basic_tms=wait_for_basic_tms,
+                    is_actualizable=True,
+                    text_field="kz_rus_ngrams_dict_pymorphy_2_4_393442_3710985",
+                    )
