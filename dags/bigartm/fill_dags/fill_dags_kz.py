b9d807bb7f3f0c8f39d435d7843ea5a16840c61e
Yakunin Kirill <yakunin.k@mail.ru>
2020-06-22 20:34:59 +0600

    change - kazakh lemmatizer - more concurrency

---
+++ dags/bigartm/fill_dags/fill_dags_kz.py
@@ -0,0 +1,71 @@
+from datetime import date
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from dags.bigartm.fill_dags.utils import gen_bigartm_operator, default_args
+
+
+def fill_dags_kz(actualizable_bigartms, comboable_bigartms):
+    dag = DAG('NLPmonitor_BigARTMs_ngramized', catchup=False, max_active_runs=1, concurrency=7,
+               default_args=default_args, schedule_interval=None)
+    with dag:
+        wait_for_basic_tms = PythonOperator(
+            task_id="wait_for_basic_tms",
+            python_callable=lambda: 0,
+        )
+
+        gen_bigartm_operator(actualizable_bigartms, comboable_bigartms, name="bigartm_test_ngram", description="Two last years", number_of_topics=200,
+                             filters={
+                                 "corpus": "main",
+                                 "source": None,
+                                 "datetime_from": None,
+                                 "datetime_to": None,
+                             },
+                             regularization_params={
+                                 "SmoothSparseThetaRegularizer": 0.15,
+                                 "SmoothSparsePhiRegularizer": 0.15,
+                                 "DecorrelatorPhiRegularizer": 0.15,
+                                 "ImproveCoherencePhiRegularizer": 0.15
+                             },
+                             wait_for_basic_tms=wait_for_basic_tms,
+                             is_actualizable=True,
+                             text_field="text_ngramized_kz_rus_ngrams_dict_pymorphy_2_4_393442_3710985",
+                             )
+
+        gen_bigartm_operator(actualizable_bigartms, comboable_bigartms, name="bigartm_two_years_ngram", description="Two last years", number_of_topics=200,
+                             filters={
+                                 "corpus": "main",
+                                 "source": None,
+                                 "datetime_from": date(2018, 6, 1),
+                                 "datetime_to": date(2020, 6, 1),
+                             },
+                             regularization_params={
+                                 "SmoothSparseThetaRegularizer": 0.15,
+                                 "SmoothSparsePhiRegularizer": 0.15,
+                                 "DecorrelatorPhiRegularizer": 0.15,
+                                 "ImproveCoherencePhiRegularizer": 0.15
+                             },
+                             wait_for_basic_tms=wait_for_basic_tms,
+                             is_actualizable=True,
+                             text_field="text_ngramized_kz_rus_ngrams_dict_pymorphy_2_4_393442_3710985",
+                             )
+
+        gen_bigartm_operator(actualizable_bigartms, comboable_bigartms, name="bigartm_two_years_ngram", description="Two last years", number_of_topics=200,
+                             filters={
+                                 "corpus": "main",
+                                 "source": None,
+                                 "datetime_from": date(2018, 6, 1),
+                                 "datetime_to": date(2020, 6, 1),
+                             },
+                             regularization_params={
+                                 "SmoothSparseThetaRegularizer": 0.15,
+                                 "SmoothSparsePhiRegularizer": 0.15,
+                                 "DecorrelatorPhiRegularizer": 0.15,
+                                 "ImproveCoherencePhiRegularizer": 0.15
+                             },
+                             wait_for_basic_tms=wait_for_basic_tms,
+                             is_actualizable=True,
+                             text_field="text_ngramized_kz_rus_ngrams_dict_pymorphy_2_4_393442_3710985",
+                             )
+    return dag
