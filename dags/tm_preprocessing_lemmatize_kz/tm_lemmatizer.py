31b726be09c825aeb7483d4ebf4c44c729a59ad6
Yakunin Kirill <yakunin.k@mail.ru>
2020-06-23 17:28:25 +0600

    change - kz lemmatizer - less concurrency

--- dags/tm_preprocessing_lemmatize_kz/tm_lemmatizer.py
+++ dags/tm_preprocessing_lemmatize_kz/tm_lemmatizer.py
@@ -32,7 +32,7 @@ with dag:
         python_callable=init_last_datetime,
     )
 
-    concurrency = 50
+    concurrency = 5
     lemmatize_operators = []
     for i in range(concurrency):
         lemmatize_operators.append(DjangoOperator(
