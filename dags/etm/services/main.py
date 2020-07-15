53e5138920acccb5bbc3b6777370ccbf6ade27c7
Sanzhar <sanzharmrz@gmail.com>
2020-07-15 16:38:46 +0600

    feat - merge train and eval parts

--- dags/etm/services/main.py
+++ dags/etm/services/main.py
@@ -8,11 +8,10 @@ def etm_calc(**kwargs):
     import numpy as np
     import os
 
-    from .data import get_batch, get_data
+    from .data import get_data
     from .model import ETM
     from .utils import train_model, visualize, evaluate
     from util.constants import BASE_DAG_DIR
-    import shutil
 
     seed = kwargs.get('seed', 666)
     corpus = kwargs.get('corpus', 'main')
@@ -37,13 +36,10 @@ def etm_calc(**kwargs):
     nonmono = kwargs.get('nonmono', 10)  # number of bad hits allowed
     wdecay = kwargs.get('wdecay', 1.2e-6)  # l2 regularization
     anneal_lr = kwargs.get('anneal_lr', 0)  # whether to anneal the learning rate or not
-    bow_norm = kwargs.get('bow_norm', 1)  # normalize bows
-    
+
     num_words = kwargs.get('num_words', 10)  # number of words for topics visualization
     visualize_every = kwargs.get('visualize_every', 10)  # print topics every n epochs
     load_from = kwargs.get('load_from', '')  # the name of model to eval from
-    tc = kwargs.get('tc', 0)  # flag for computing topic coherence
-    td = kwargs.get('td', 0)  # flag for computing topic diversity
 
     load_from = os.path.join(BASE_DAG_DIR, save_path, load_from)
     save_path = os.path.join(BASE_DAG_DIR, save_path)
@@ -60,7 +56,6 @@ def etm_calc(**kwargs):
     # 1. vocabulary
     data_path = os.path.join(BASE_DAG_DIR, 'etm_temp')
     vocab, train, test = get_data(data_path)
-    shutil.rmtree(data_path, ignore_errors=True)
     vocab_size = len(vocab)
 
     # 1. training data
@@ -137,69 +132,45 @@ def etm_calc(**kwargs):
         print('Defaulting to vanilla SGD')
         optimizer = optim.SGD(model.parameters(), lr=lr)
 
-    if mode == 'train':
-        # train model on data
-        best_val_ppl = 1e9
-        all_val_ppls = []
-        print('\n')
-        print('Visualizing model quality before training...')
-        visualize(m=model, vocab=vocab, num_topics=num_topics, num_words=num_words, save_path=save_path)
-        print('\n')
-        for epoch in range(1, epochs):
-            train_model(model=model, epoch=epoch, optimizer=optimizer)
-            val_ppl = evaluate(m=model, source='val', vocab=vocab)
-            if val_ppl < best_val_ppl:
-                with open(ckpt, 'wb') as f:
-                    torch.save(model, f)
-                best_val_ppl = val_ppl
-            else:
-                # check whether to anneal lr
-                lr = optimizer.param_groups[0]['lr']
-                if anneal_lr and (
-                        len(all_val_ppls) > nonmono and val_ppl > min(all_val_ppls[:-nonmono]) and lr > 1e-5):
-                    optimizer.param_groups[0]['lr'] /= lr_factor
-            if epoch % visualize_every == 0:
-                visualize(m=model, vocab=vocab, num_topics=num_topics, num_words=num_words, save_path=save_path)
-            all_val_ppls.append(val_ppl)
-        with open(ckpt, 'rb') as f:
-            model = torch.load(f)
-        model = model.to(device)
-    else:
-        with open(ckpt, 'rb') as f:
-            model = torch.load(f)
-        model = model.to(device)
-        model.eval()
+    # train model on data
+    best_val_ppl = 1e9
+    all_val_ppls = []
+    print('\n')
+    print('Visualizing model quality before training...')
+    visualize(m=model, vocab=vocab, num_topics=num_topics, num_words=num_words, save_path=save_path)
+    print('\n')
+    for epoch in range(1, epochs):
+        train_model(model=model, epoch=epoch, optimizer=optimizer)
+        val_ppl = evaluate(m=model, source='train', vocab=vocab)
+        if val_ppl < best_val_ppl:
+            with open(ckpt, 'wb') as f:
+                torch.save(model, f)
+            best_val_ppl = val_ppl
+        else:
+            # check whether to anneal lr
+            lr = optimizer.param_groups[0]['lr']
+            if anneal_lr and (
+                    len(all_val_ppls) > nonmono and val_ppl > min(all_val_ppls[:-nonmono]) and lr > 1e-5):
+                optimizer.param_groups[0]['lr'] /= lr_factor
+        if epoch % visualize_every == 0:
+            visualize(m=model, vocab=vocab, num_topics=num_topics, num_words=num_words, save_path=save_path)
+        all_val_ppls.append(val_ppl)
+    with open(ckpt, 'rb') as f:
+        model = torch.load(f)
+    model = model.to(device)
 
-        with torch.no_grad():
-            test_ppl = evaluate(m=model, source='test', tc=tc, td=td, vocab=vocab)
-            # get most used topics
-            indices = torch.tensor(range(num_docs_train))
-            indices = torch.split(indices, batch_size)
-            thetaAvg = torch.zeros(1, num_topics).to(device)
-            thetaWeightedAvg = torch.zeros(1, num_topics).to(device)
-            cnt = 0
-            for idx, ind in enumerate(indices):
-                data_batch = get_batch(train_tokens, train_counts, ind, vocab_size, device)
-                sums = data_batch.sum(1).unsqueeze(1)
-                cnt += sums.sum(0).squeeze().cpu().numpy()
-                if bow_norm:
-                    normalized_data_batch = data_batch / sums
-                else:
-                    normalized_data_batch = data_batch
-                theta, _ = model.get_theta(normalized_data_batch)
-                thetaAvg += theta.sum(0).unsqueeze(0) / num_docs_train
-                weighed_theta = sums * theta
-                thetaWeightedAvg += weighed_theta.sum(0).unsqueeze(0)
-                if idx % 100 == 0 and idx > 0:
-                    print('batch: {}/{}'.format(idx, len(indices)))
-            thetaWeightedAvg = thetaWeightedAvg.squeeze().cpu().numpy() / cnt
-            print('\nThe 10 most used topics are {}'.format(thetaWeightedAvg.argsort()[::-1][:10]))
+    model.eval()
 
-            # show topics
-            beta = model.get_beta()
-            print('\n')
-            for k in range(num_topics):  # topic_indices:
-                gamma = beta[k]
-                top_words = list(gamma.cpu().numpy().argsort()[-num_words + 1:][::-1])
-                topic_words = [vocab[a] for a in top_words]
-                print('Topic {}: {}'.format(k, topic_words))
+    with torch.no_grad():
+        test_ppl, topic_coh, topic_div = evaluate(m=model, source='test', tc=True, td=True, vocab=vocab)
+        print(f'!!! TEST PERPLEXITY {test_ppl}')
+        # show topics
+        beta = model.get_beta()
+        print('\n')
+        for k in range(num_topics):  # topic_indices:
+            gamma = beta[k]
+            top_words = list(gamma.cpu().numpy().argsort()[-num_words + 1:][::-1])
+            topic_words = [vocab[a] for a in top_words]
+            print('Topic {}: {}'.format(k, topic_words))
+
+    return 666
