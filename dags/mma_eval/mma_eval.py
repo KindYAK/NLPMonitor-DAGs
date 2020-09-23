f800d9dd61541837d49a99de699cd263a3191dd7
Yakunin Kirill <yakunin.k@mail.ru>
2020-09-23 14:14:57 +0600

    change - mma_eval - task for survey results with multiple classes

--- dags/mma_eval/mma_eval.py
+++ dags/mma_eval/mma_eval.py
@@ -20,8 +20,9 @@ default_args = {
 
 dag = DAG('Calc_mma_eval', catchup=False, max_active_runs=1, default_args=default_args, schedule_interval=None)
 
+mmas = []
 with dag:
-    evaluator = DjangoOperator(
+    mmas.append(DjangoOperator(
         task_id=f"calculate_mma",
         python_callable=calc_mma,
         op_kwargs={
@@ -31,4 +32,28 @@ with dag:
             "class_ids": (36, ),
             "perform_actualize": False
         }
-    )
+    ))
+
+    mmas.append(DjangoOperator(
+        task_id=f"calculate_mma_surveys",
+        python_callable=calc_mma,
+        op_kwargs={
+            "topic_modelling_name": "bigartm_two_years_main_and_gos2",
+            "criterion_ids": (1, 35, 34, 37), # Тональность (негатив!!), Резонансность, Гос. программы, Опросы
+            "criterion_weights": (
+                (0.44, 0.33, 0, 0.23),
+                (0, 0.2, 0.4, 0.4),
+                (-0.44, 0.33, 0, 0.23),
+                (0.36, 0.27, 0.18, 0.18),
+                (-0.36, 0.27, 0.18, 0.18),
+            ),
+            "class_ids": (
+                38,
+                39,
+                40,
+                41,
+                42,
+            ),
+            "perform_actualize": False
+        }
+    ))
