c53aaa3f75ff5849082b8a23f5214de3a9bcd6f9
SanzharMrz <sanzharmrz@gmail.com>
2020-09-29 18:10:07 +0600

    fix - import issues

--- util/igramscraper/two_step_verification/__init__.py
+++ util/igramscraper/two_step_verification/__init__.py
@@ -1,4 +1,5 @@
-from igramscraper.two_step_verification.two_step_verification_abstract_class import TwoStepVerificationAbstractClass
-from igramscraper.two_step_verification.console_verification import ConsoleVerification
+from util.igramscraper.two_step_verification.two_step_verification_abstract_class import \
+    TwoStepVerificationAbstractClass
+from util.igramscraper.two_step_verification.console_verification import ConsoleVerification
 
 __all__ = ["TwoStepVerificationAbstractClass", "ConsoleVerification"]
