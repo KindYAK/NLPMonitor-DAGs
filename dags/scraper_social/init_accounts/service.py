47f96a0b64df4b179ca151a392cc02e450816dca
Yakunin Kirill <yakunin.k@mail.ru>
2020-10-13 15:32:43 +0600

    feat - scraping - init DAG - save monitoring objects to Airflow Variable

--- dags/scraper_social/init_accounts/service.py
+++ dags/scraper_social/init_accounts/service.py
@@ -1,7 +1,18 @@
 def init_accounts(**kwargs):
     from airflow.models import Variable
-    from scraping.models import SocialNetworkAccount
+    from scraping.models import SocialNetworkAccount, MonitoringQuery
     import json
+
+    Variable.set("social_networks",
+                     json.dumps(
+                         [
+                             {
+                                 "id": s[0],
+                                 "name": s[1],
+                             } for s in SocialNetworkAccount.SOCIAL_NETWORKS
+                         ]
+                     )
+                 )
 
     ss = SocialNetworkAccount.objects.filter(is_active=True)
     Variable.set("social_accounts",
@@ -20,13 +31,18 @@ def init_accounts(**kwargs):
                      )
                  )
 
-    Variable.set("social_networks",
+    mqs = MonitoringQuery.objects.filter(is_active=True)
+    Variable.set("monitoring_queries",
                      json.dumps(
                          [
                              {
-                                 "id": s[0],
-                                 "name": s[1],
-                             } for s in SocialNetworkAccount.SOCIAL_NETWORKS
+                                 "id": mq.id,
+                                 "query": mq.query,
+                                 "priority_rate": mq.priority_rate,
+                                 # Get datetime back with datetime.datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S")
+                                 "datetime_last_parsed": mq.datetime_last_parsed.isoformat()[:-6] if mq.datetime_last_parsed else None,
+                                 "social_network": mq.social_network,
+                             } for mq in mqs
                          ]
                      )
                  )
