8bbda9edfb9eb9a6c85b83a6857ecf5509fda9a0
SanzharMrz <sanzharmrz@gmail.com>
2020-10-14 19:37:30 +0600

    fix - get fields from video_meta

--- dags/scraper_social/scrap/youtube/service.py
+++ dags/scraper_social/scrap/youtube/service.py
@@ -2,7 +2,7 @@ import datetime
 from django.utils import timezone
 
 date_getter = lambda x: datetime.datetime.fromtimestamp(x['video_publish_date']).replace(tzinfo=timezone.utc)
-text_getter = lambda x: x['text']
+text_getter = lambda x: x['video_description']
 
 
 def scrap_youtube_async(yt_api, account, auth_account, datetime_last=None):
@@ -14,7 +14,7 @@ def scrap_youtube_async(yt_api, account, auth_account, datetime_last=None):
         from django.utils import timezone
         while True:
             try:
-                posts = yt_api.search(channel_id=account.account_id, max_results=100, order_by='date')
+                posts = yt_api.search(channel_id=account.account_id, max_results=30, order_by='date')
             except Exception as e:
                 print(e)
                 auth_account.datetime_videos_limit_reached = timezone.now()
@@ -35,8 +35,8 @@ def scrap_youtube_async(yt_api, account, auth_account, datetime_last=None):
         hashtags_list = video_meta['video_tags'].split('|')
 
         comments_list = list()
-        if message['video_comment_count']:
-            comments = yt_api.get_video_comments(video_id=message['video_id'], max_results=100)  # list of dicts
+        if video_meta['video_comment_count']:
+            comments = yt_api.get_video_comments(video_id=message['video_id'], max_results=50)  # list of dicts
             comments_list = parse_yt_comments(comments_dict_list=comments)  # comment_text, comment_date, comment_id
 
         return create_document(
@@ -46,8 +46,8 @@ def scrap_youtube_async(yt_api, account, auth_account, datetime_last=None):
             text=message['video_description'],
             datetime=date_getter(message),
             num_comments=video_meta['video_comment_count'],
-            num_likes=message['video_like_count'],
-            num_views=message['video_view_count'],
+            num_likes=video_meta['video_like_count'],
+            num_views=video_meta['video_view_count'],
             url=f"{account.id}-{message['video_id']}",
             hashtags_list=hashtags_list,
             comments_list=comments_list
@@ -65,17 +65,17 @@ def scrap_youtube_async(yt_api, account, auth_account, datetime_last=None):
             print(f"{account.id}-{message['video_id']} not found!")
             raise e
 
-        if message['video_comment_count'] > d.num_comments:
-            comments_to_add = message['video_comment_count'] - d.num_comments
+        if video_meta['video_comment_count'] > d.num_comments:
+            comments_to_add = video_meta['video_comment_count'] - d.num_comments
             comments = yt_api.get_video_comments(video_id=message['video_id'], max_results=comments_to_add)
             comments_list = parse_yt_comments(comments_dict_list=comments)
             create_comments(comments_list=comments_list, document=d)
 
         d.num_comments = video_meta['video_comment_count']
-        d.num_likes = message['video_like_count'],
-        d.num_views = message['video_view_count']
+        d.num_likes = video_meta['video_like_count'],
+        d.num_views = video_meta['video_view_count']
         d.save()
 
     loop = asyncio.get_event_loop()
-    return loop.run_until_complete(scrap_wrapper_async(account, iterator, document_handler, document_updater,
+    return loop.run_until_complete(scrap_wrapper_async(account, iterator(), document_handler, document_updater,
                                                               date_getter, text_getter, datetime_last))
