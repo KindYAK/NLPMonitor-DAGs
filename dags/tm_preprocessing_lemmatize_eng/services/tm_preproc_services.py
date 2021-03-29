35bc12ae06995c9909ade7cab4c807ddca23ab2d
Yakunin Kirill <yakunin.k@mail.ru>
2021-03-29 19:31:44 +0600

    change - English lemmatization - increase max batch to 100000

--- dags/tm_preprocessing_lemmatize_eng/services/tm_preproc_services.py
+++ dags/tm_preprocessing_lemmatize_eng/services/tm_preproc_services.py
@@ -31,10 +31,10 @@ def preprocessing_raw_data(**kwargs):
     s = s.exclude('exists', field="is_english")
     start = int(start / 100 * number_of_documents)
     end = int(end / 100 * number_of_documents) + 1
-    if end - start < 30000:
+    if end - start < 100_000:
         s = s[start:end]
     else:
-        s = s[start:start+30000]
+        s = s[start:start+100_000]
     documents = s.execute()
 
     stopwords = set(get_stop_words('ru') + get_stop_words('en') + stopwords.words('english'))
