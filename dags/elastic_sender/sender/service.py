39bf8407f9ca586384433cc4518188f6ba8c39fd
Yakunin Kirill <yakunin.k@mail.ru>
2021-03-29 19:17:05 +0600

    sender - fix 1 mln limit (types issue)

--- dags/elastic_sender/sender/service.py
+++ dags/elastic_sender/sender/service.py
@@ -6,8 +6,8 @@ def send_elastic(**kwargs):
     from_id = Variable.get("send_elastic_from_id", default_var=0)
     to_id = Document.objects.latest('id').id
 
-    if to_id - from_id > 1_000_000:
-        to_id = from_id + 1_000_000
+    if to_id - int(from_id) > 1_000_000:
+        to_id = int(from_id) + 1_000_000
 
     # Send_elastic call
     Command().handle(batch_size=1000, from_id=from_id, to_id=to_id)
