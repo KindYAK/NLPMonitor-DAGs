2e45e3ca1af3ef6e0c5061222a72a9918371eb2f
Yakunin Kirill <yakunin.k@mail.ru>
2021-04-05 20:00:00 +0600

    lemmatizer_eng - more concurrency

--- dags/tm_preprocessing_lemmatize_eng/tm_lemmatizer.py
+++ dags/tm_preprocessing_lemmatize_eng/tm_lemmatizer.py
@@ -24,7 +24,7 @@ default_args = {
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG('Nlpmonitor_Lemmatization_eng', catchup=False, max_active_runs=1, concurrency=4, default_args=default_args, schedule_interval='* * * * *')
+dag = DAG('Nlpmonitor_Lemmatization_eng', catchup=False, max_active_runs=1, concurrency=16, default_args=default_args, schedule_interval='* * * * *')
 
 with dag:
     # init_last_datetime = DjangoOperator(
@@ -34,7 +34,7 @@ with dag:
     #     }
     # )
 
-    concurrency = 4
+    concurrency = 16
     lemmatize_operators = []
     for i in range(concurrency):
         lemmatize_operators.append(DjangoOperator(
