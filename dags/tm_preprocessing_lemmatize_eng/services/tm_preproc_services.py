ecc54c55b2020d99424fda974ee655175d30c78e
Yakunin Kirill <yakunin.k@mail.ru>
2021-04-05 19:02:15 +0600

    fix

--- dags/tm_preprocessing_lemmatize_eng/services/tm_preproc_services.py
+++ dags/tm_preprocessing_lemmatize_eng/services/tm_preproc_services.py
@@ -33,7 +33,7 @@ def preprocessing_raw_data(**kwargs):
     stopwords = set(get_stop_words('ru') + get_stop_words('en') + stopwords.words('english'))
     success = 0
     documents = []
-    for doc in s.scan(raise_on_error=False):
+    for doc in s.params(raise_on_error=False).scan():
         if int(doc.id) % total_proc != process_num:
             continue
         success += 1
