1852da75fb260c3ef10d593e1a0d2a05d28f2ecd
KindYAK <yakunin.k@mail.ru>
2021-04-13 14:43:54 +0600

    TM - health - kaz+rus TM

--- dags/bigartm/fill_dags/fill_dags_healthcare.py
+++ dags/bigartm/fill_dags/fill_dags_healthcare.py
@@ -109,4 +109,23 @@ def fill_dags_healthcare(actualizable_bigartms, comboable_bigartms):
                              is_actualizable=True,
                              text_field="text_ngramized_kz_rus_yandex_ngrams_dict")
 
+        gen_bigartm_operator(actualizable_bigartms, comboable_bigartms, name="bigartm_2020_2021_rus_kaz_health_1",
+                             description="2020-2021", number_of_topics=150,
+                             filters={
+                                 "corpus": ["main", "rus", "rus_propaganda"],
+                                 "source": None,
+                                 "datetime_from": date(2020, 1, 1),
+                                 "datetime_to": date(2021, 4, 15),
+                                 "group_id": 104,
+                             },
+                             regularization_params={
+                                 "SmoothSparseThetaRegularizer": 0.15,
+                                 "SmoothSparsePhiRegularizer": 0.15,
+                                 "DecorrelatorPhiRegularizer": 0.15,
+                                 "ImproveCoherencePhiRegularizer": 0.15
+                             },
+                             wait_for_basic_tms=wait_for_basic_tms,
+                             is_actualizable=True,
+                             text_field="text_ngramized_kz_rus_yandex_ngrams_dict")
+
     return dag
