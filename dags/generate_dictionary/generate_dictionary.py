3bde7ad0f3f25812c0984f587f93987a81a5cabf
KindYAK <yakunin.k@mail.ru>
2021-04-16 19:54:44 +0600

    dictionary_generation - increase concurrency 5 -> 8

--- dags/generate_dictionary/generate_dictionary.py
+++ dags/generate_dictionary/generate_dictionary.py
@@ -21,7 +21,7 @@ default_args = {
     'pool': 'long_tasks'
 }
 
-dag = DAG('Nlpmonitor_Dictionary_Generation', catchup=False, concurrency=5, max_active_runs=1, default_args=default_args, schedule_interval=None)
+dag = DAG('Nlpmonitor_Dictionary_Generation', catchup=False, concurrency=8, max_active_runs=1, default_args=default_args, schedule_interval=None)
 
 with dag:
     corpuses = ["scopus_real_real"]
