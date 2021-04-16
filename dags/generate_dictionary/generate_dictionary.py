3b07578019cdb3cde027212afcf67e27742c7614
KindYAK <yakunin.k@mail.ru>
2021-04-16 19:25:48 +0600

    dictionary_generation - bigger chunks, but also less concurrency

--- dags/generate_dictionary/generate_dictionary.py
+++ dags/generate_dictionary/generate_dictionary.py
@@ -21,7 +21,7 @@ default_args = {
     'pool': 'long_tasks'
 }
 
-dag = DAG('Nlpmonitor_Dictionary_Generation', catchup=False, concurrency=10, max_active_runs=1, default_args=default_args, schedule_interval=None)
+dag = DAG('Nlpmonitor_Dictionary_Generation', catchup=False, concurrency=5, max_active_runs=1, default_args=default_args, schedule_interval=None)
 
 with dag:
     corpuses = ["scopus_real_real"]
@@ -44,7 +44,7 @@ with dag:
         }
     )
 
-    concurrency = 200
+    concurrency = 100
     dictionary_operators = []
     for i in range(concurrency):
         dictionary_operators.append(DjangoOperator(
