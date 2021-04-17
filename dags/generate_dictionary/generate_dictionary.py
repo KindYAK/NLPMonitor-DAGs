013c9776fba4fb4cd52d573d4ace1b5c7826ecb8
KindYAK <yakunin.k@mail.ru>
2021-04-17 13:37:06 +0600

    dictionary gen - changes

--- dags/generate_dictionary/generate_dictionary.py
+++ dags/generate_dictionary/generate_dictionary.py
@@ -21,7 +21,7 @@ default_args = {
     'pool': 'long_tasks'
 }
 
-dag = DAG('Nlpmonitor_Dictionary_Generation', catchup=False, concurrency=6, max_active_runs=1, default_args=default_args, schedule_interval=None)
+dag = DAG('Nlpmonitor_Dictionary_Generation', catchup=False, concurrency=8, max_active_runs=1, default_args=default_args, schedule_interval=None)
 
 with dag:
     corpuses = ["scopus_real_real"]
@@ -44,7 +44,7 @@ with dag:
         }
     )
 
-    concurrency = 200
+    concurrency = 150
     dictionary_operators = []
     for i in range(concurrency):
         dictionary_operators.append(DjangoOperator(
