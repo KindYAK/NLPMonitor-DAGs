5c65d4b24ddb2663fe73d295d3e5bd71b2ae522f
KindYAK <yakunin.k@mail.ru>
2021-04-17 13:36:33 +0600

    dictionary gen - changes

--- dags/generate_dictionary/services/service.py
+++ dags/generate_dictionary/services/service.py
@@ -191,8 +191,8 @@ def generate_dictionary_batch(**kwargs):
                         dictionary_words[word]['document_frequency'] += 1
                 word_in_doc.add(word)
 
-    len_dictionary = len(dictionary_words)
     dictionary_words = filter(lambda x: x['document_frequency'] > number_of_documents * min_relative_document_frequency, dictionary_words.values())
+    len_dictionary = len(dictionary_words)
     success = 0
     failed = 0
     print("!!!", "Writing to ES", datetime.datetime.now())
