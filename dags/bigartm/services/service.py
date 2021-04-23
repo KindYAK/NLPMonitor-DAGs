75a324d64815ffb5c0ed04aa617e759c94d15022
KindYAK <yakunin.k@mail.ru>
2021-04-23 16:10:01 +0600

    TM generator - fix clean function (import)

--- dags/bigartm/services/service.py
+++ dags/bigartm/services/service.py
@@ -260,10 +260,11 @@ def dataset_prepare(**kwargs):
 
     formated_data = document_scanner(s, text_field, corpus, ids_to_skip, group_document_es_ids)
 
-    # try: # TEMP!!
-    peek_doc = next(formated_data)
-    # except:
-    #     peek_doc = False
+    try:
+        peek_doc = next(formated_data)
+    except Exception as e:
+        print("!!! No docs", e)
+        peek_doc = False
     if perform_actualize and not peek_doc:
         return f"No documents to actualize"
 
