ffe14238c3631709869c22ad8dee8d20ff325868
KindYAK <yakunin.k@mail.ru>
2021-04-23 17:05:13 +0600

    fix

--- dags/bigartm/services/service.py
+++ dags/bigartm/services/service.py
@@ -287,7 +287,7 @@ def dataset_prepare(**kwargs):
     artm.BatchVectorizer(data_path=os.path.join(data_folder, f"bigartm_formated_data.txt"),
                          data_format="vowpal_wabbit",
                          target_folder=os.path.join(data_folder, "batches"))
-    return f"index.number_of_document={index.number_of_documents}, len(ids)={len(ids)}"
+    return f"index.number_of_document={index.number_of_documents}"
 
 
 def topic_modelling(**kwargs):
