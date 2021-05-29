ca8d3299179bafc95ea2c0e48ce87d8babdabb65
KindYAK <yakunin.k@mail.ru>
2021-05-29 11:14:53 +0600

    limit of bigartm batch - 1 -> 4 mln, but remove 500-topic model from fast actualizing

--- dags/bigartm_actualizer/bigartm_actualizer.py
+++ dags/bigartm_actualizer/bigartm_actualizer.py
@@ -64,7 +64,7 @@ fast_tms = [
     "bigartm_education_2_2020",
     "bigartm_education_3_2020",
     "bigartm__scopus_100",
-    "bigartm__scopus_500",
+    # "bigartm__scopus_500",
 ]
 actualizers_calcs_fast = []
 with dag_fast:
