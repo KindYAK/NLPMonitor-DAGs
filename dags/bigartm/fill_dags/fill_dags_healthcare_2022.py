aab512148cdcdfcb42885b27ad17e374bcd54b85
KindYAK <yakunin.k@mail.ru>
2022-02-23 15:17:39 +0600

    new TM

--- dags/bigartm/fill_dags/fill_dags_healthcare_2022.py
+++ dags/bigartm/fill_dags/fill_dags_healthcare_2022.py
@@ -52,65 +52,23 @@ def fill_dags_healthcare_2022(actualizable_bigartms, comboable_bigartms):
 
 
         # # FIRST LEVEL
-        # gen_bigartm_operator(actualizable_bigartms, comboable_bigartms, name="bigartm_2020_2021_rus_kaz_health_1",
-        #                      description="2020-2021", number_of_topics=150,
-        #                      filters={
-        #                          "corpus": ["main", "rus", "rus_propaganda"],
-        #                          "source": None,
-        #                          "datetime_from": date(2020, 1, 1),
-        #                          "datetime_to": date(2021, 4, 15),
-        #                          "group_id": 104,
-        #                      },
-        #                      regularization_params={
-        #                          "SmoothSparseThetaRegularizer": 0.15,
-        #                          "SmoothSparsePhiRegularizer": 0.15,
-        #                          "DecorrelatorPhiRegularizer": 0.15,
-        #                          "ImproveCoherencePhiRegularizer": 0.15
-        #                      },
-        #                      wait_for_basic_tms=wait_for_basic_tms,
-        #                      is_actualizable=False,
-        #                      text_field="text_ngramized_kz_rus_yandex_ngrams_dict")
-        #
-        # # SECOND LEVEL
-        # gen_bigartm_operator(actualizable_bigartms, comboable_bigartms, name="bigartm_2020_2021_rus_kaz_health_2",
-        #                      description="2020-2021", number_of_topics=100,
-        #                      filters={
-        #                          "corpus": ["main", "rus", "rus_propaganda"],
-        #                          "source": None,
-        #                          "datetime_from": date(2020, 1, 1),
-        #                          "datetime_to": date(2021, 4, 15),
-        #                          "group_id": 107,
-        #                          "topic_weight_threshold": 0.04,
-        #                      },
-        #                      regularization_params={
-        #                          "SmoothSparseThetaRegularizer": 0.15,
-        #                          "SmoothSparsePhiRegularizer": 0.15,
-        #                          "DecorrelatorPhiRegularizer": 0.15,
-        #                          "ImproveCoherencePhiRegularizer": 0.15
-        #                      },
-        #                      wait_for_basic_tms=wait_for_basic_tms,
-        #                      is_actualizable=False,
-        #                      text_field="text_ngramized_kz_rus_yandex_ngrams_dict")
-        #
-        # # THIRD LEVEL
-        # gen_bigartm_operator(actualizable_bigartms, comboable_bigartms, name="bigartm_2020_2021_rus_kaz_health_3",
-        #                      description="2020-2021", number_of_topics=50,
-        #                      filters={
-        #                          "corpus": ["main", "rus", "rus_propaganda"],
-        #                          "source": None,
-        #                          "datetime_from": date(2020, 1, 1),
-        #                          "datetime_to": date(2021, 4, 20),
-        #                          "group_id": 110,
-        #                          "topic_weight_threshold": 0.1,
-        #                      },
-        #                      regularization_params={
-        #                          "SmoothSparseThetaRegularizer": 0.15,
-        #                          "SmoothSparsePhiRegularizer": 0.15,
-        #                          "DecorrelatorPhiRegularizer": 0.15,
-        #                          "ImproveCoherencePhiRegularizer": 0.15
-        #                      },
-        #                      wait_for_basic_tms=wait_for_basic_tms,
-        #                      is_actualizable=False,
-        #                      text_field="text_ngramized_kz_rus_yandex_ngrams_dict")
+        gen_bigartm_operator(actualizable_bigartms, comboable_bigartms, name="bigartm_2020_2022_rus_kaz_health_1",
+                             description="2020-2022", number_of_topics=150,
+                             filters={
+                                 "corpus": "main",
+                                 "source": None,
+                                 "datetime_from": date(2020, 1, 1),
+                                 "datetime_to": date(2021, 4, 15),
+                                 "group_id": 211,
+                             },
+                             regularization_params={
+                                 "SmoothSparseThetaRegularizer": 0.15,
+                                 "SmoothSparsePhiRegularizer": 0.15,
+                                 "DecorrelatorPhiRegularizer": 0.15,
+                                 "ImproveCoherencePhiRegularizer": 0.15
+                             },
+                             wait_for_basic_tms=wait_for_basic_tms,
+                             is_actualizable=False,
+                             text_field="text_ngramized_kz_rus_yandex_ngrams_dict")
 
     return dag
